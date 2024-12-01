ce1dd8eb50a8aa9bc8eb6935c0b36d24aa52cbba
Arpit-Singh320 <arpit2005singh@gmail.com>
2024-12-02 01:51:13 +0530

    Update settings.py

--- clinic_backend/clinic_backend/settings.py
+++ clinic_backend/clinic_backend/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()  # Reading .env file
+
+DATABASES = {
+    'default': env.db()  # Uses DATABASE_URL from environment variables
+}
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
